2a3109d5799efe06de92e2d7b890e2be4879fff8
Dennis Adrian <adriangbetancur@gmail.com>
2020-09-12 21:43:42 -0400

    aprendiendo useState() y useEffect()

--- src/components/Molecules/CourseCard.jsx
+++ src/components/Molecules/CourseCard.jsx
@@ -23,36 +23,36 @@ import { Link } from 'react-router-dom';
 
 //===================USING specific attributes
 const CourseCard = ({ id, image, title, price }) => (
-  <article className="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <Link to={`/cursos/${id}`}>
-        <img src={image} alt={title} />
-      </Link>
-    </div>
-    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="center">{title}</h3>
-      <div className="s-main-center">
-        <a
-          className="button--ghost-alert button--tiny"
-          href="http://dennisadrian.me"
-        >
-          {`$ ${price} USD`}
-        </a>
-      </div>
-    </div>
-  </article>
+    <article className="card">
+        <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+            <Link to={`/cursos/${id}`}>
+                <img src={image} alt={title} />
+            </Link>
+        </div>
+        <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+            <h3 className="center">{title}</h3>
+            <div className="s-main-center">
+                <a
+                    className="button--ghost-alert button--tiny"
+                    href="http://dennisadrian.me"
+                >
+                    {`$ ${price} USD`}
+                </a>
+            </div>
+        </div>
+    </article>
 );
 
 CourseCard.propTypes = {
-  title: PropTypes.string,
-  image: PropTypes.string,
-  price: PropTypes.number,
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
 };
 CourseCard.defaultProps = {
-  title: 'No se encontró título',
-  image:
-    'https://images.pexels.com/photos/4406222/pexels-photo-4406222.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  price: '--',
+    title: 'No se encontró título',
+    image:
+        'https://images.pexels.com/photos/4406222/pexels-photo-4406222.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    price: '--',
 };
 
 export default CourseCard;
--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,55 +1,39 @@
-import React from "react";
-
-const cursos = [
-    {
-        id: 1,
-        title: 'React Desde Cero',
-        image:
-            'https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg',
-        price: 20,
-        professor: 'Beto Quiroga',
-    },
-    {
-        id: 2,
-        title: 'APIs con .NET Core',
-        image:
-            'https://edteam-media.s3.amazonaws.com/courses/small/fe19dd8c-ff56-4e26-b5bd-904f3631855b.png',
-        price: 30,
-        professor: 'Miguel Teheran',
-    },
-    {
-        id: 3,
-        title: 'JavaScript Desde Cero',
-        image:
-            'https://edteam-media.s3.amazonaws.com/courses/small/61e5a210-8dab-412e-a6dc-802c070cc18c.jpg',
-        price: 20,
-        professor: 'Beto Quiroga',
-    },
-    {
-        id: 4,
-        title: 'HTML Desde Cero',
-        image:
-            'https://edteam-media.s3.amazonaws.com/courses/small/26557907-0555-427e-a40c-6ff207f98d72.png',
-        price: 40,
-        professor: 'Álvaro Felipe',
-    },
-];
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Course = ({ match }) => {
+    //con los Hooks, podemos tener múltiples estados para nuestra app
+    //state, y setState son simplemente nombres estándar pero pueden tener cualquier nombre
+    const [course, setCourse] = useState({})
+    //estado para los comentarios
+    const [comment, setComment] = useState("Sin comentarios")
 
-    const cursoActual = cursos.filter(curso => curso.id === parseInt(match.params.id))[0];
+    useEffect(() => {
+        axios.get(`http://my-json-server.typicode.com/dennis-adrian/json-db/courses/${match.params.id}`)
+            .then(res => setCourse(res.data))
+    }, [])
+
+
+    const myComment = e => {
+        setComment(e.target.value)
+    }
 
     return (
-        cursoActual ? (
-            <div className="ed-grid m-grid-3">
-                <h1 className="m-cols-3">{cursoActual.title}</h1>
-                <img className="m-cols-1" src={cursoActual.image} alt={cursoActual.title} />
-                <p className="m-cols-2">Profesor: {cursoActual.professor}</p>
+        course ? (
+            <div className="ed-grid">
+                <div className="l-block">
+                    <h1 className="m-cols-3">{course.title}</h1>
+                    <img className="m-cols-1" src={course.image} alt={course.title} />
+                    <p className="m-cols-2">Profesor: {course.professor}</p>
+                </div>
+                <div>
+                    <h2>Escribe tu comentario</h2>
+                    <input type="text" name="" id="" placeholder="Escribe..." onChange={myComment.bind(this)}/>
+                    <p>{comment}</p>
+                </div>
             </div>
         ) : (
-                <div className="ed-grid">
-                    <h1>El curso no existe</h1>
-                </div>
+                <h1>El curso no existe</h1>
             )
     )
 }
